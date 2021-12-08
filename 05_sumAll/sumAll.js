df0ce100eb429c147d0ec2be7477ec02c25da323
Cody <whearcody@gmail.com>
2021-12-08 15:05:15 +0000

    Easier than the last one, this still required some research. Using new functions such as parseInt was good, as well as Math.min and max. ENjoyable again.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+function sumAll(one, two) {
+    let total = 0;
+    if (one < 0 || two < 0) {
+        return "ERROR";
+    }
+    if (one !== parseInt(one,10) || two !== parseInt(two,10)) {
+        return "ERROR";
+    }
+    for (let i = Math.min(one, two); i <= Math.max(one, two); i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
