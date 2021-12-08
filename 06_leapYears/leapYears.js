f1d99b7e07d906030a19f8cf25830ada8653af49
Cody <whearcody@gmail.com>
2021-12-08 16:48:21 +0000

    Challenge 6 involved figuring out if a year was a leap year, this simply used a few if statements as well as .isInteger.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+function leapYears(input) {
+    if (Number.isInteger(input / 400)) {
+        return true;
+    }   else  if (Number.isInteger(input / 100) && Number.isInteger(input / 4)) {
+        return false;
+    } else if (Number.isInteger(input / 4)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
