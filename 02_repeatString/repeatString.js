10b94a443c9e64db880eb20691766794475014ee
Cody <whearcody@gmail.com>
2021-12-08 12:40:27 +0000

    Exercise 2 invovled returning a string a set number of times, intially a headache until I realised return ended a for loop. Something learned :)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+let string = "hey";
+let num = 3;
 
-};
-
+function repeatString(string, num) {
+    let words = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++){
+        words += string;
+    }
+    return words;
+}
 // Do not edit below this line
 module.exports = repeatString;
