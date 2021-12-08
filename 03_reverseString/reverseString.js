ab65d0db8215fe8a07125f16aabf326f87af727e
Cody <whearcody@gmail.com>
2021-12-08 12:56:28 +0000

    Exercise 3 was challening and involved some research. I had to basically reverse a string, and I managed to do it in one line pretty much. It involved putting the string elements to an array, reversing the array, taking it out of an array with join() and returning the value. Very fun

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function(str) {
+let str = "12345";
 
+
+function reverseString(str) {
+    const dissolved = str.split("").reverse().join("");
+
+    return dissolved;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
