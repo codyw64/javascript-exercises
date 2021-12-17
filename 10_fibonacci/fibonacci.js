7655944311eb94919f2a943aca64d9484de3d413
Cody <whearcody@gmail.com>
2021-12-17 13:19:12 +0000

    Breezed through fibonacci in no time, maths background helped, not 100% sure my solutions were optimal but they worked.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    banana = parseInt(number);
+    if (banana < 0) { return "OOPS"}
+    if (banana == 1 || banana == 2) {return 1;}
+    let a = 1;
+    let b = 1;
+    let c;
+    for (let i = 1; i < banana - 1; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
