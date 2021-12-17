1dd8735481a86cc64e295818456db66f784c1aa8
Cody <whearcody@gmail.com>
2021-12-17 16:25:22 +0000

    Completed all of the exercises up to 12, on 12 i did google and copy paste but I get how it works. I struggled with sorting for people that didnt have a year of death, I didnt know how to calculate each persons age and actually use it.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (people) {
+    const age = (x) => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
+    const oldest = people.sort((a, b) =>
+      age(b) - age(a)
+    );
+    return oldest[0];
+  };
 // Do not edit below this line
 module.exports = findTheOldest;
