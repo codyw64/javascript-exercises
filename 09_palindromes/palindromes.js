22c46ae5e25746d785c217caecb9ebca2d4ece52
Cody <whearcody@gmail.com>
2021-12-17 13:04:47 +0000

    Completed, not in record time and struggled a little, but I was searching for a better solution than one I have done before.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let reduced = word.replace(/[.!, ]/g, "").toLowerCase().split("");
+    let rev = word.replace(/[.!, ]/g, "").toLowerCase().split("").reverse();
+    console.log(reduced);
+    console.log(rev);
+    for (let i = 0; i < reduced.length; i++) {
+        if (reduced[i] !== rev[i]) {
+            console.log(reduced[i]);
+            return false;
+        } else {return true}
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
